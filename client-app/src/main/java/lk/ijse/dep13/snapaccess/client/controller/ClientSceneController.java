f42756287543378bb72eea012114a9e74e7b153b
lahiru-sandhamal <lahiru2k14@gmail.com>
2025-01-10 13:33:17 +0530

    ClientSceneController Update

--- client-app/src/main/java/lk/ijse/dep13/snapaccess/client/controller/ClientSceneController.java
+++ client-app/src/main/java/lk/ijse/dep13/snapaccess/client/controller/ClientSceneController.java
@@ -5,19 +5,25 @@ import java.io.OutputStream;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class MainSceneController {
+public class ClientSceneController {
 
-    public void initialize() throws IOException {
-        Socket socket = new Socket("127.0.0.1", 9090);
-        OutputStream os = socket.getOutputStream();
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            System.out.print("Enter your message: ");
-            String message = scanner.nextLine();
-            os.write(("Lahiru Sandhamal: " + message).getBytes());
-            os.flush();
+    public void initialize() {
+        // Run terminal interaction in a separate thread
+        new Thread(() -> {
+            try {
+                Socket socket = new Socket("127.0.0.1", 5050);
+                OutputStream os = socket.getOutputStream();
+                Scanner scanner = new Scanner(System.in);
 
-
-        }
+                while (true) {
+                    System.out.print("Enter your message: ");
+                    String message = scanner.nextLine();
+                    os.write(("Lahiru Sandhamal: " + message).getBytes());
+                    os.flush();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 }
--- server-app/src/main/java/lk/ijse/dep13/snapaccess/server/SeverAppInitializer.java
+++ server-app/src/main/java/lk/ijse/dep13/snapaccess/server/SeverAppInitializer.java
@@ -1,4 +1,37 @@
 package lk.ijse.dep13.snapaccess.server;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class SeverAppInitializer {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(5050);
+        System.out.println("Server started on port 5050");
+        while (true) {
+            System.out.println("Waiting for connection..."  );
+            Socket localsocket = serverSocket.accept();
+            new Thread(() -> {
+                try{
+                    System.out.println("Accepted connection from ");
+                    InputStream is = localsocket.getInputStream();
+                    while (true){
+                        byte[] buffer = new byte[1024];
+                        int len = is.read(buffer);
+                        System.out.println(new String(buffer,0,len));
+                    }
+
+                } catch (Exception e) {
+
+                }
+            }).start();
+
+
+
+
+
+
+        }
+    }
 }
