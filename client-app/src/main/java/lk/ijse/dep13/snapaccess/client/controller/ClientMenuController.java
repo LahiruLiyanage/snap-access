6520bf39475180c1ea254aafe0ed532bcd8af1fb
Lahiru Liyanage <lhlahiru95@gmail.com>
2025-01-11 14:11:20 +0530

    Add Client Side Menu(FXML,css and Controller)

---
+++ client-app/src/main/java/lk/ijse/dep13/snapaccess/client/controller/ClientMenuController.java
@@ -0,0 +1,37 @@
+package lk.ijse.dep13.snapaccess.client.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class ClientMenuController {
+    public Button btnChat;
+    public Button btnConnect;
+    public Button btnLogout;
+    public Button btnReport;
+    public Button btnScreenShare;
+    public Button btnScreenShare1;
+    public Button btnVideoCall;
+    public TextField txtIpAddress;
+
+    public void btnChatOnAction(ActionEvent event) {
+
+    }
+
+    public void btnConnectOnAction(ActionEvent event) {
+
+    }
+
+    public void btnReportOnAction(ActionEvent event) {
+
+    }
+
+    public void btnScreenShareOnAction(ActionEvent event) {
+
+    }
+
+    public void btnVideoCallOnAction(ActionEvent event) {
+
+    }
+
+}
