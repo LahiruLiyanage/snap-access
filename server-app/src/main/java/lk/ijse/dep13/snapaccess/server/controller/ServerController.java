f607b33862eacde7d5045f074be8f0bf232a3b57
Lahiru Liyanage <lhlahiru95@gmail.com>
2025-01-18 16:08:39 +0530

    Update ServerController.java, ServerMenuController.java, ServerMenu.FXML

--- server-app/src/main/java/lk/ijse/dep13/snapaccess/server/controller/ServerController.java
+++ server-app/src/main/java/lk/ijse/dep13/snapaccess/server/controller/ServerController.java
@@ -1,4 +1,46 @@
 package lk.ijse.dep13.snapaccess.server.controller;
 
-public class ServerController {
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import lk.ijse.dep13.snapaccess.server.util.AppRouter;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ServerController implements Initializable {
+    public BorderPane server_parent;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/scene/ServerMenu.fxml"));
+            Pane menu = loader.load();
+
+            ServerMenuController menuController = loader.getController();
+
+            menuController.setServerController(this);
+
+            server_parent.setLeft(menu);
+
+            setCenterContent(AppRouter.Routes.DASHBOARD);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void setCenterContent(AppRouter.Routes route) {
+        try {
+            Pane newContent = AppRouter.getContainer(route);
+
+            server_parent.setCenter(newContent);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
--- server-app/src/main/java/lk/ijse/dep13/snapaccess/server/controller/ServerMenuController.java
+++ server-app/src/main/java/lk/ijse/dep13/snapaccess/server/controller/ServerMenuController.java
@@ -1,4 +1,54 @@
 package lk.ijse.dep13.snapaccess.server.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import lk.ijse.dep13.snapaccess.server.util.AppRouter;
+
 public class ServerMenuController {
+    public Button btnChat;
+    public Button btnDashBoard;
+    public Button btnFileShare;
+    public Button btnLogout;
+    public Button btnReport;
+    public Button btnScreenShare;
+    public Button btnVideoCall;
+
+    private ServerController severController; // Reference to ClientController
+
+    public void setServerController(ServerController clientController) {
+        this.severController = clientController;
+    }
+
+    public void btnChatOnAction(ActionEvent event) {
+        navigate(AppRouter.Routes.TEXTING);
+    }
+
+    public void btnDashBoardOnAction(ActionEvent event) {
+        navigate(AppRouter.Routes.DASHBOARD);
+    }
+
+    public void btnFileShareOnAction(ActionEvent event) {
+        navigate(AppRouter.Routes.FILE_TRANSFER);
+    }
+
+    public void btnReportOnAction(ActionEvent event) {
+        navigate(AppRouter.Routes.DASHBOARD);
+    }
+
+    public void btnScreenShareOnAction(ActionEvent event) {
+        navigate(AppRouter.Routes.SCREENSHARE);
+    }
+
+    public void btnVideoCallOnAction(ActionEvent event) {
+        navigate(AppRouter.Routes.VIDEO_CALL);
+    }
+
+    private void navigate(AppRouter.Routes route) {
+        if (severController != null) {
+            severController.setCenterContent(route);
+        } else {
+            System.err.println("ServerController is not set!");
+        }
+    }
+
 }
